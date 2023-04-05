77c5b5c048f7579dbc9b9b87c52d726f962eb5d8
Risky Kurniawan <riskykurniawan15@gmail.com>
2023-04-05 21:52:53 +0700

    init app hello word

---
+++ src/test/java/com/example/helloProjectRK/HelloProjectRkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloProjectRK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloProjectRkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
